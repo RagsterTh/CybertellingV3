1960dd9961a75834f75560ea07de390b0eed4202
RagsterTh <139650575+RagsterTh@users.noreply.github.com>
2026-08-14 15:17:24 -0300

    Mais ajustes

--- Assets/Scripts/ObjectSfxAndAnimation.cs
+++ Assets/Scripts/ObjectSfxAndAnimation.cs
@@ -40,14 +40,7 @@ public class ObjectSfxAndAnimation : MonoBehaviour
         if (audioSource != null && audioSource.isPlaying)
             return;
 
-<<<<<<< Updated upstream
-        if (ExperienceMnager.Instance.AllTargetsFound())
-        {
-            ExperienceMnager.Instance.OnGameFinish?.Invoke();
-        }
-=======
         ExperienceMnager.Instance.OnGameFinish?.Invoke();
->>>>>>> Stashed changes
         gameObject.SetActive(false);
     }
     private void PlayActivationAnimation()
